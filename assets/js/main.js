53e2e5377d136e0fdaa3165b1f38e52684c5b550
Yan-Yi Li <liyanyi@gmail.com>
2016-08-25 21:23:27 +0100

    Add smooth scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
-$(document).foundation();
+$(document).ready(function($) {
+  $(document).foundation();
 
-$('#search').click(function() {
-  $('#search-container').on('open.zf.reveal', function() {
-    $('#search-input').focus();
+  // focus search input
+  $('#search, #mobile-search').click(function() {
+    $('#search-container').on('open.zf.reveal', function() {
+      $('#search-input').focus();
+    });
+  });
+
+  // smooth scrolling
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
+      }
+    }
   });
 });
